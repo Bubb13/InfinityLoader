Bubb13/InfinityLoader

commit 80a57067c9cd8e76a8745c5be1b372b21c3951d5

Bindings for saving throw hooks

LuaBindings-v2.6.6.0/headers/LuaBindings-v2.6.6.0/Baldur-v2.6.6.0_generated.h
@@ -7,8 +7,22 @@
 #include "InfinityLoaderCommon.h"
 #include "to_lua_pointers.h"
 
+typedef char sbyte;
 typedef unsigned short ushort;
 typedef unsigned int uint;
+
+template<typename T>
+struct hasDestruct {
+    template<typename U>
+    static constexpr auto check(U*) -> decltype(std::declval<U>().Destruct(), false) {
+        return true;
+    }
+    template<typename>
+    static constexpr bool check(...) {
+        return false;
+    }
+    static constexpr bool value = check<T>(nullptr);
+};
 
 template<typename A>
 struct W
@@ -36,9 +50,18 @@ public:
 	}
 
 	~W() {
-		val.Destruct();
+		if constexpr (hasDestruct<decltype(val)>::value) {
+			val.Destruct();
+		}
 	}
 };
+
+template<typename T, typename... Args>
+T* newEngineObj(Args&&... args) {
+	T* ptr = reinterpret_cast<T*>(p_malloc(sizeof(T)));
+	ptr->Construct(std::forward<Args>(args)...);
+	return ptr;
+}
 
 struct ALCcontext_struct;
 struct ALCdevice_struct;
@@ -95,6 +118,7 @@ struct CRes;
 struct CResPVR;
 struct CResRef;
 struct CResTileSet;
+struct CRuleTables;
 struct CSavedGamePartyCreature;
 struct CScreenStore;
 struct CScreenWorld;
@@ -107,6 +131,7 @@ struct CSoundMixerImp;
 struct CSpawn;
 struct CSpell;
 struct CString;
+struct CTlkTable;
 struct CUIControlTextDisplay;
 struct CVVCHashEntry;
 struct CVariable;
@@ -123,6 +148,7 @@ struct SDL_BlitMap;
 union SDL_Event;
 struct SDL_PixelFormat;
 struct SDL_Window;
+struct STR_RES;
 struct Spell_Header_st;
 struct Spell_ability_st;
 struct SteamUGCDetails_t;
@@ -2781,6 +2807,8 @@ namespace EEex
 	void DestroyUDAux(lua_State* L, void* ptr);
 
 	void CopyUDAux(lua_State* L, void* sourcePtr, void* targetPtr);
+
+	int Override_CGameEffect_CheckSave(CGameEffect* pEffect, CGameSprite* pSprite, byte* saveVSDeathRollRaw, byte* saveVSWandsRollRaw, byte* saveVSPolyRollRaw, byte* saveVSBreathRollRaw, byte* saveVSSpellRollRaw, byte* resistMagicRollRaw);
 };
 
 struct ConstCharString
@@ -4263,17 +4291,43 @@ struct CString
 	typedef void (__thiscall *type_ConstructFromChars)(CString* pThis, const char* lpsz);
 	static type_ConstructFromChars p_ConstructFromChars;
 
+	typedef void (__thiscall *type_ConstructFromCString)(CString* pThis, const CString* other);
+	static type_ConstructFromCString p_ConstructFromCString;
+
 	typedef void (__thiscall *type_Destruct)(CString* pThis);
 	static type_Destruct p_Destruct;
+
+	typedef void (__thiscall *type_SetFromChars)(const CString* pThis, const char* lpsz);
+	static type_SetFromChars p_SetFromChars;
+
+	typedef void (__thiscall *type_SetFromCString)(const CString* pThis, const CString* other);
+	static type_SetFromCString p_SetFromCString;
 
 	void Construct(const char* lpsz)
 	{
 		p_ConstructFromChars(this, lpsz);
 	}
 
+	void Construct(const CString* other)
+	{
+		p_ConstructFromCString(this, other);
+	}
+
 	void Destruct()
 	{
 		p_Destruct(this);
+	}
+
+	void Construct();
+
+	void operator=(const char* lpsz) const
+	{
+		p_SetFromChars(this, lpsz);
+	}
+
+	void operator=(const CString* other) const
+	{
+		p_SetFromCString(this, other);
 	}
 };
 
@@ -4602,20 +4656,20 @@ struct CPoint
 
 	CPoint()
 	{
-		this->x = 0;
-		this->y = 0;
+		x = 0;
+		y = 0;
 	}
 
 	CPoint(int x, int y)
 	{
-		this->x = x;
-		this->y = y;
+		x = x;
+		y = y;
 	}
 
 	CPoint(CPoint* point)
 	{
-		this->x = point->x;
-		this->y = point->y;
+		x = point->x;
+		y = point->y;
 	}
 };
 
@@ -5709,6 +5763,78 @@ struct CMessageSetDirection : CMessage
 		m_sourceId = caller;
 		m_targetId = target;
 		m_face = face;
+	}
+};
+
+struct CMessageDisplayTextRef : CMessage
+{
+	struct vtbl : CMessage::vtbl
+	{
+		vtbl() = delete;
+	};
+
+	static void* VFTable;
+	unsigned int m_name;
+	unsigned int m_text;
+	unsigned int m_nameColor;
+	unsigned int m_textColor;
+	int m_marker;
+	unsigned __int8 m_moveToTop;
+	unsigned int m_overHead;
+	unsigned __int8 m_overrideDialogMode;
+	unsigned __int8 m_bPlaySound;
+
+	CMessageDisplayTextRef() = delete;
+
+	void Construct(uint name, uint text, uint nameColor, uint textColor, int marker, int caller, int target)
+	{
+		*reinterpret_cast<void**>(this) = VFTable;
+		m_targetId = target;
+		m_sourceId = caller;
+		m_name = name;
+		m_text = text;
+		m_nameColor = nameColor;
+		m_textColor = textColor;
+		m_marker = marker;
+		m_moveToTop = 0;
+		m_overHead = 0;
+		m_overrideDialogMode = 0;
+		m_bPlaySound = 1;
+	}
+};
+
+struct CMessageDisplayText : CMessage
+{
+	struct vtbl : CMessage::vtbl
+	{
+		vtbl() = delete;
+	};
+
+	static void* VFTable;
+	CString m_name;
+	CString m_text;
+	unsigned int m_nameColor;
+	unsigned int m_textColor;
+	int m_marker;
+	unsigned __int8 m_moveToTop;
+	unsigned __int8 m_overHead;
+	unsigned __int8 m_overrideDialogMode;
+
+	CMessageDisplayText() = delete;
+
+	void Construct(CString* name, CString* text, uint nameColor, uint textColor, int marker, int caller, int target)
+	{
+		*reinterpret_cast<void**>(this) = VFTable;
+		m_targetId = target;
+		m_sourceId = caller;
+		m_name.Construct(name);
+		m_text.Construct(text);
+		m_nameColor = nameColor;
+		m_textColor = textColor;
+		m_marker = marker;
+		m_moveToTop = 0;
+		m_overHead = 0;
+		m_overrideDialogMode = 0;
 	}
 };
 
@@ -7215,6 +7341,14 @@ struct CTlkTable
 	unsigned int m_nEngineStringBase;
 
 	CTlkTable() = delete;
+
+	typedef byte (__thiscall *type_Fetch)(CTlkTable* pThis, uint strId, STR_RES* strRes, int ignoreSTRREFON);
+	static type_Fetch p_Fetch;
+
+	byte Fetch(uint strId, STR_RES* strRes, int ignoreSTRREFON)
+	{
+		return p_Fetch(this, strId, strRes, ignoreSTRREFON);
+	}
 };
 
 struct CStoreFileSpell
@@ -8292,6 +8426,14 @@ struct CRuleTables
 	Array<int,56> m_speechNums;
 
 	CRuleTables() = delete;
+
+	typedef byte (__thiscall *type_MapCharacterSpecializationToSchool)(CRuleTables* pThis, ushort nSpecialistMage);
+	static type_MapCharacterSpecializationToSchool p_MapCharacterSpecializationToSchool;
+
+	byte MapCharacterSpecializationToSchool(ushort nSpecialistMage)
+	{
+		return p_MapCharacterSpecializationToSchool(this, nSpecialistMage);
+	}
 };
 
 struct CAIScriptFile
@@ -9128,6 +9270,22 @@ struct CSound : CObject, CResHelper<CResWave,4>
 	CSoundImp* pimpl;
 
 	CSound() = delete;
+
+	typedef void (__thiscall *type_Construct)(CSound* pThis);
+	static type_Construct p_Construct;
+
+	typedef void (__thiscall *type_Destruct)(CSound* pThis);
+	static type_Destruct p_Destruct;
+
+	void Construct()
+	{
+		p_Construct(this);
+	}
+
+	void Destruct()
+	{
+		p_Destruct(this);
+	}
 };
 
 struct CScreenChapter : CBaldurEngine
@@ -9202,6 +9360,18 @@ struct STR_RES
 	CSound cSound;
 
 	STR_RES() = delete;
+
+	void Construct()
+	{
+		szText.Construct();
+		cSound.Construct();
+	}
+
+	void Destruct()
+	{
+		cSound.Destruct();
+		szText.Destruct();
+	}
 };
 
 struct CWeather
@@ -9956,6 +10126,14 @@ struct CItem : CResHelper<CResItem,1005>
 	CGameEffectUsabilityList m_Usability;
 
 	CItem() = delete;
+
+	typedef void (__thiscall *type_Construct3)(CItem* pThis, CResRef id, ushort useCount1, ushort useCount2, ushort useCount3, ushort wear, uint flags);
+	static type_Construct3 p_Construct3;
+
+	void Construct(CResRef id, ushort useCount1, ushort useCount2, ushort useCount3, ushort wear, uint flags)
+	{
+		p_Construct3(this, id, useCount1, useCount2, useCount3, wear, flags);
+	}
 
 	virtual void virtual_Destruct()
 	{
@@ -12010,13 +12188,13 @@ struct CAIObjectType
 
 	void Construct(const CAIObjectType* toCopy)
 	{
-		this->m_name.m_pchData = *p_afxPchNil;
-		this->Set(toCopy);
+		m_name.Construct();
+		Set(toCopy);
 	}
 
 	void Destruct()
 	{
-		this->m_name.Destruct();
+		m_name.Destruct();
 	}
 };
 
@@ -13805,6 +13983,12 @@ struct CGameSprite : CGameAIBase
 	typedef void (__thiscall *type_UpdateTarget)(CGameSprite* pThis, CGameObject* target);
 	static type_UpdateTarget p_UpdateTarget;
 
+	typedef uint (__thiscall *type_GetNameRef)(CGameSprite* pThis);
+	static type_GetNameRef p_GetNameRef;
+
+	typedef CString* (__thiscall *type_GetName)(CGameSprite* pThis, int ignoreSTRREFON);
+	static type_GetName p_GetName;
+
 	short GetCasterLevel(CSpell* pSpell, int includeWildMage)
 	{
 		return p_GetCasterLevel(this, pSpell, includeWildMage);
@@ -13863,6 +14047,16 @@ struct CGameSprite : CGameAIBase
 	void UpdateTarget(CGameObject* target)
 	{
 		p_UpdateTarget(this, target);
+	}
+
+	uint GetNameRef()
+	{
+		return p_GetNameRef(this);
+	}
+
+	CString* GetName(int ignoreSTRREFON)
+	{
+		return p_GetName(this, ignoreSTRREFON);
 	}
 
 	virtual void virtual_SetTarget_2(const CPoint* _0, int _1)
@@ -14022,5 +14216,9 @@ struct CContingency
 	CContingency() = delete;
 };
 
+inline void CString::Construct()
+{
+	m_pchData = *p_afxPchNil;
+}
 
 extern std::vector<std::pair<const TCHAR*, void**>> internalPointersMap;

scripts/generate_bindings/generate_bindings.py
@@ -1128,6 +1128,7 @@ class FunctionImplementation:
 		self.returnType: TypeReference = None
 		self.callingConvention: str = None
 		self.name: str = None
+		self.funcPtrName: str = None
 		self.removedThisType: TypeReference = None
 		self.parameters: list[FunctionImplementationParameter] = []
 		self.body: list[str] = []
@@ -1142,7 +1143,21 @@ class FunctionImplementation:
 		self.virtual: bool = False
 		self.externalImplementation: bool = False
 		self.passLuaState: bool = False
+		self.eofBody: bool = False
+		self.bindingName: str = None
 
+
+	def setName(self, name: str):
+		self.name = name
+		# This is normally called after setBindingName().
+		# Only replace funcPtrName if it wasn't set by
+		# setBindingName().
+		if self.funcPtrName == None:
+			self.funcPtrName = name.replace("=", "_equ")
+
+	def setBindingName(self, bindingName: str):
+		self.bindingName = bindingName
+		self.funcPtrName = bindingName
 
 	def getAllTypeReferences(self, mainState: MainState):
 
@@ -1161,11 +1176,14 @@ class FunctionImplementation:
 		return refs
 
 
-	def toString(self, mainState: MainState, indent=""):
+	def toString(self, mainState: MainState, indent="", eof: bool = False):
 
 		parts: list[str] = [indent]
 		if self.group != mainState.globalGroup:
-			parts.append("\t")
+			if not eof:
+				parts.append("\t")
+			else:
+				parts.append("inline ")
 
 		if self.virtual:
 			parts.append("virtual ")
@@ -1173,13 +1191,17 @@ class FunctionImplementation:
 		if self.isStatic:
 			parts.append("static ")
 
+		namespace = f"{self.group.name}::" if self.eofBody and eof else ""
+
 		if self.isFakeConstructor:
-			parts.append(f"{self.returnType.getHeaderName()} Construct")
+			parts.append(f"{self.returnType.getHeaderName()} {namespace}Construct")
 		elif not self.isConstructor and not self.isDestructor:
 			parts.append(self.returnType.getHeaderName())
 			parts.append(" ")
+			parts.append(namespace)
 			parts.append(self.name)
 		else:
+			parts.append(namespace)
 			if self.isDestructor: parts.append("~")
 			parts.append(self.group.singleName)
 
@@ -1215,11 +1237,14 @@ class FunctionImplementation:
 
 		elif not self.noBody:
 			assert not self.externalImplementation, "Function cannot be header-defined and externally-defined"
-			parts.append("\n")
-			for bodyLine in self.body:
-				parts.append(indent)
-				parts.append(bodyLine)
+			if not self.eofBody or eof:
 				parts.append("\n")
+				for bodyLine in self.body:
+					parts.append(indent)
+					parts.append(bodyLine if not eof else bodyLine[1:])
+					parts.append("\n")
+			else:
+				parts.append(";\n")
 
 		elif not self.externalImplementation:
 
@@ -1230,7 +1255,7 @@ class FunctionImplementation:
 			if not self.returnType.isVoid():
 				parts.append("return ")
 
-			parts.append(f"{indent}p_{self.name}(")
+			parts.append(f"{indent}p_{self.funcPtrName}(")
 
 			if not self.isStatic:
 				parts.append("this")
@@ -1274,7 +1299,7 @@ variablePatternLocal = "^\t((?:(?:nopointer|nobinding|static)\s+)*)(?:__unaligne
 
 def processCommonGroupLines(mainState: MainState, state: CheckLinesState, line: str, group: Group):
 
-	functionImplementationMatch: Match = re.match("^\s*(?!typedef)((?:(?:\$nobinding|\$nodeclaration|\$external_implementation|\$pass_lua_state)\s+)*)(?:(static)\s+){0,1}([, _a-zA-Z0-9*&:<>$]+?)\s+(?:(__cdecl|__stdcall|__thiscall)\s+){0,1}([_a-zA-Z0-9\[\]]+)\s*\(\s*((?:[, _a-zA-Z0-9*:<>]+?\s+[_a-zA-Z0-9]+(?:\s*,(?!\s*\))){0,1})*)\s*\)\s*(const){0,1}\s*(?:(;)){0,1}$", line)
+	functionImplementationMatch: Match = re.match("^\s*(?!typedef)((?:(?:\$nobinding|\$nodeclaration|\$external_implementation|\$pass_lua_state|\$eof_body|\$binding_name\(\S+\))\s+)*)(?:(static)\s+){0,1}([, _a-zA-Z0-9*&:<>$]+?)\s+(?:(__cdecl|__stdcall|__thiscall)\s+){0,1}([_a-zA-Z0-9\[\]=]+)\s*\(\s*((?:[, _a-zA-Z0-9*:<>]+?\s+[_a-zA-Z0-9]+(?:\s*,(?!\s*\))){0,1})*)\s*\)\s*(const){0,1}\s*(?:(;)){0,1}$", line)
 	if functionImplementationMatch:
 
 		state.currentFunctionImplementation = FunctionImplementation()
@@ -1294,6 +1319,14 @@ def processCommonGroupLines(mainState: MainState, state: CheckLinesState, line: 
 				elif keyword == "$pass_lua_state":
 					assert not state.currentFunctionImplementation.passLuaState, "pass_lua_state already defined"
 					state.currentFunctionImplementation.passLuaState = True
+				elif keyword == "$eof_body":
+					assert not state.currentFunctionImplementation.eofBody, "eof_body already defined"
+					state.currentFunctionImplementation.eofBody = True
+				elif bindingNameMatch := re.match("^\$binding_name\((\S+)\)$", keyword):
+					assert not state.currentFunctionImplementation.bindingName, "bindingName already defined"
+					state.currentFunctionImplementation.setBindingName(bindingNameMatch.group(1))
+				else:
+					assert False, "Bad bindings directive"
 
 		state.currentFunctionImplementation.isStatic = functionImplementationMatch.group(2) != None
 
@@ -1319,7 +1352,7 @@ def processCommonGroupLines(mainState: MainState, state: CheckLinesState, line: 
 		state.currentFunctionImplementation.returnType = defineTypeRef(mainState, group, retTypeStr, TypeRefSourceType.FUNCTION, debugLine=f"processCommonGroupLines()-1 {line}")
 		state.currentFunctionImplementation.callingConvention = functionImplementationMatch.group(4)
 
-		state.currentFunctionImplementation.name = functionImplementationMatch.group(5)
+		state.currentFunctionImplementation.setName(functionImplementationMatch.group(5))
 
 		parameterStr = functionImplementationMatch.group(6)
 		if parameterStr != None and parameterStr != "":
@@ -2044,10 +2077,11 @@ class Group:
 
 				if functionImp.noBody and not functionImp.externalImplementation:
 
-					realVariableName: str = f"{group.name}::{functionImp.name}" if isNormal else functionImp.name
-					pointerVariableName: str = f"{group.name}::p_{functionImp.name}" if isNormal else f"p_{functionImp.name}"
+					funcName: str = functionImp.name if not functionImp.bindingName else functionImp.bindingName
+					realVariableName: str = f"{group.name}::{funcName}" if isNormal else functionImp.name
+					pointerVariableName: str = f"{group.name}::p_{functionImp.funcPtrName}" if isNormal else f"p_{functionImp.funcPtrName}"
 
-					pointerVariableTypeName: str = f"type_{functionImp.name}"
+					pointerVariableTypeName: str = f"type_{functionImp.funcPtrName}"
 					pointerVariableFullTypeName: str = f"{group.name}::{pointerVariableTypeName}" if isNormal else pointerVariableTypeName
 
 					internalPointersOut.write(f"{pointerVariableFullTypeName} {pointerVariableName};\n")
@@ -2075,7 +2109,7 @@ class Group:
 
 						parts.append(");\n")
 						modifierStr: str = "static" if isNormal else "extern"
-						parts.append(f"{indent}{modifierStr} {pointerVariableTypeName} p_{functionImp.name};")
+						parts.append(f"{indent}{modifierStr} {pointerVariableTypeName} p_{functionImp.funcPtrName};")
 						parts.append("\n\n")
 						wroteSomething = True
 
@@ -3126,9 +3160,13 @@ def writeBindings(mainState: MainState, outputFileName: str, groups: UniqueList[
 			isNormal: bool = group != mainState.globalGroup
 			functionOpenData = OpenFunctionData()
 
-			functionOpenData.functionName = functionImplementation.name
+			funcImpBindingName: str = functionImplementation.name \
+				if not functionImplementation.bindingName \
+				else functionImplementation.bindingName
+
+			functionOpenData.functionName = funcImpBindingName
 			groupIden = f"{groupNameStrIden}_" if isNormal else ""
-			functionOpenData.functionBindingName = resolveIdentifer(f"tolua_function_{groupIden}{functionImplementation.name}")
+			functionOpenData.functionBindingName = resolveIdentifer(f"tolua_function_{groupIden}{funcImpBindingName}")
 
 			groupOpenData.functionBindings.append(functionOpenData)
 
@@ -3136,7 +3174,7 @@ def writeBindings(mainState: MainState, outputFileName: str, groups: UniqueList[
 			out.write("{\n")
 
 			if isNormal and group.groupType != "namespace" and not functionImplementation.isStatic:
-				writeAccessSelf(f"invalid 'self' in calling function '{functionImplementation.name}'")
+				writeAccessSelf(f"invalid 'self' in calling function '{funcImpBindingName}'")
 
 			callArgParts: list[str] = []
 			def checkPrimitiveHandling(paramType: TypeReference, luaVarIndex: int):
@@ -3153,23 +3191,23 @@ def writeBindings(mainState: MainState, outputFileName: str, groups: UniqueList[
 
 				if checkType.isPrimitiveNumber() and checkType.getUserTypePointerLevel() == 0:
 					if paramType.getName() in ("float", "double"):
-						callArgParts.append(f"{enumCastString}tolua_function_tonumber(L, {luaVarIndex}, \"{functionImplementation.name}\")")
+						callArgParts.append(f"{enumCastString}tolua_function_tonumber(L, {luaVarIndex}, \"{funcImpBindingName}\")")
 					else:
-						callArgParts.append(f"{enumCastString}tolua_function_tointeger(L, {luaVarIndex}, \"{functionImplementation.name}\")")
+						callArgParts.append(f"{enumCastString}tolua_function_tointeger(L, {luaVarIndex}, \"{funcImpBindingName}\")")
 					callArgParts.append(", ")
 					return True
 				elif checkTypeName == "bool" and checkType.getUserTypePointerLevel() == 0:
-					callArgParts.append(f"tolua_function_toboolean(L, {luaVarIndex}, \"{functionImplementation.name}\")")
+					callArgParts.append(f"tolua_function_toboolean(L, {luaVarIndex}, \"{funcImpBindingName}\")")
 					callArgParts.append(", ")
 					return True
 				elif checkTypeName == "char" and not checkType.unsigned:
 					if checkType.getUserTypePointerLevel() == 1:
 						nonConstCast = "(char*)" if not checkType.const else ""
-						callArgParts.append(f"{nonConstCast}tolua_function_tostring(L, {luaVarIndex}, \"{functionImplementation.name}\")")
+						callArgParts.append(f"{nonConstCast}tolua_function_tostring(L, {luaVarIndex}, \"{funcImpBindingName}\")")
 						callArgParts.append(", ")
 						return True
 					elif checkType.getUserTypePointerLevel() == 0:
-						callArgParts.append(f"tolua_function_tochar(L, {luaVarIndex}, \"{functionImplementation.name}\")")
+						callArgParts.append(f"tolua_function_tochar(L, {luaVarIndex}, \"{funcImpBindingName}\")")
 						callArgParts.append(", ")
 						return True
 
@@ -3178,7 +3216,7 @@ def writeBindings(mainState: MainState, outputFileName: str, groups: UniqueList[
 			out.write("\t")
 
 			parameterIMod: int = None
-			functionNameHeader = f"p_{functionImplementation.name}" \
+			functionNameHeader = f"p_{functionImplementation.funcPtrName}" \
 				if (not isNormal and functionImplementation.noBody and not functionImplementation.externalImplementation) \
 				else functionImplementation.name
 
@@ -3737,9 +3775,24 @@ def outputHeader(mainState: MainState, outputFileName: str, out: TextIOWrapper):
 
 		out.write("\n")
 
+		def doEOFFuncOutput(group: Group):
+
+			for subgroup in group.subGroups:
+				doEOFFuncOutput(subgroup)
+
+			for functionImp in group.functionImplementations:
+				if functionImp.eofBody:
+					out.write(functionImp.toString(mainState, eof=True))
+					out.write("\n")
+
+		# Output eof body functions
+		for group in mainState.filteredGroups:
+			if group.isSubgroup() or group.ignoreHeader or group.groupType == "undefined": continue
+			doEOFFuncOutput(group)
+
 		if internalPointersOut:
 
-			out.write("\nextern std::vector<std::pair<const TCHAR*, void**>> internalPointersMap;\n")
+			out.write("extern std::vector<std::pair<const TCHAR*, void**>> internalPointersMap;\n")
 
 			if len(internalPointersList) > 0:
 				internalPointersOut.write("\n")

InfinityLoaderCommon/headers/common/InfinityLoaderCommon.h
@@ -49,6 +49,7 @@ typedef std::wifstream IFStream;
 
 typedef std::string StringA;
 typedef std::wstring StringW;
+typedef std::ostringstream OStringStreamA;
 
 /////////////
 // Globals //

commit b2bffa572ecc87060fe8c6b22074096a9f0186c6

Fix CPoint regression in 80a5706

LuaBindings-v2.6.6.0/headers/LuaBindings-v2.6.6.0/Baldur-v2.6.6.0_generated.h
@@ -4669,8 +4669,8 @@ struct CPoint
 
 	CPoint(int x, int y)
 	{
-		x = x;
-		y = y;
+		this->x = x;
+		this->y = y;
 	}
 
 	CPoint(CPoint* point)

scripts/generate_bindings/generate_bindings.py
@@ -9,7 +9,7 @@ from enum import Enum
 from io import TextIOWrapper
 from itertools import islice
 from typing import Callable, Generic, Match, Pattern, Tuple, TypeVar
-import importlib
+import importlib.util
 import re
 import sys
 