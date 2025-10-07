Bubb13/InfinityLoader

commit b67cf26b27302f059262d6abf22ffc7feb219199

Fix Infinity_TransitionMenu() fade time with uncapped FPS

EEex-v2.6.6.0/source/EEex-v2.6.6.0/Generated/EEexLua_generated.cpp
@@ -316,7 +316,6 @@ static void tolua_reg_types(lua_State* L)
 	tolua_usertype(L, "EEex::ProjectileType");
 	tolua_usertype(L, "CRes");
 	tolua_usertype(L, "CResWorldMap");
-	tolua_usertype(L, "CResWebm");
 	tolua_usertype(L, "CResWave");
 	tolua_usertype(L, "CResWED");
 	tolua_usertype(L, "CResUI");
@@ -340,6 +339,7 @@ static void tolua_reg_types(lua_State* L)
 	tolua_usertype(L, "CResBitmap");
 	tolua_usertype(L, "CResBIO");
 	tolua_usertype(L, "CResArea");
+	tolua_usertype(L, "CResWebm");
 	tolua_usertype(L, "CAOEEntry");
 	tolua_usertype(L, "CSpawnPoint");
 	tolua_usertype(L, "CPersistantEffect");
@@ -753,9 +753,6 @@ int OpenBindingsInternal(lua_State* L)
 	tolua_cclass(L, "CResWorldMap", "CResWorldMap", {"CRes"}, NULL);
 	tolua_beginmodule(L, "CResWorldMap");
 	tolua_endmodule(L);
-	tolua_cclass(L, "CResWebm", "CResWebm", {"CRes"}, NULL);
-	tolua_beginmodule(L, "CResWebm");
-	tolua_endmodule(L);
 	tolua_cclass(L, "CResWave", "CResWave", {"CRes"}, NULL);
 	tolua_beginmodule(L, "CResWave");
 	tolua_endmodule(L);
@@ -824,6 +821,9 @@ int OpenBindingsInternal(lua_State* L)
 	tolua_endmodule(L);
 	tolua_cclass(L, "CResArea", "CResArea", {"CRes"}, NULL);
 	tolua_beginmodule(L, "CResArea");
+	tolua_endmodule(L);
+	tolua_cclass(L, "CResWebm", "CResWebm", {"CRes"}, NULL);
+	tolua_beginmodule(L, "CResWebm");
 	tolua_endmodule(L);
 	tolua_cclass(L, "CAOEEntry", "CAOEEntry", {}, NULL);
 	tolua_beginmodule(L, "CAOEEntry");

LuaBindings-v2.6.6.0/source/LuaBindings-v2.6.6.0/Generated/EEexLua_generated_baseclass_offsets.cpp
@@ -17,9 +17,6 @@ void registerBaseclasses() {
 		{"CRainDrop", {
 			{"CParticle", offsetofbase<CRainDrop, CParticle>()},
 		}},
-		{"CRect", {
-			{"tagRECT", offsetofbase<CRect, tagRECT>()},
-		}},
 		{"CSize", {
 			{"tagSIZE", offsetofbase<CSize, tagSIZE>()},
 		}},
@@ -31,12 +28,6 @@ void registerBaseclasses() {
 		}},
 		{"CResWorldMap::vtbl", {
 			{"CRes::vtbl", offsetofbase<CResWorldMap::vtbl, CRes::vtbl>()},
-		}},
-		{"CResWebm", {
-			{"CRes", offsetofbase<CResWebm, CRes>()},
-		}},
-		{"CResWebm::vtbl", {
-			{"CRes::vtbl", offsetofbase<CResWebm::vtbl, CRes::vtbl>()},
 		}},
 		{"CResWave", {
 			{"CRes", offsetofbase<CResWave, CRes>()},
@@ -175,6 +166,12 @@ void registerBaseclasses() {
 		}},
 		{"CResArea::vtbl", {
 			{"CRes::vtbl", offsetofbase<CResArea::vtbl, CRes::vtbl>()},
+		}},
+		{"CResWebm", {
+			{"CRes", offsetofbase<CResWebm, CRes>()},
+		}},
+		{"CResWebm::vtbl", {
+			{"CRes::vtbl", offsetofbase<CResWebm::vtbl, CRes::vtbl>()},
 		}},
 		{"CArray<CRect,CRect*>", {
 			{"CObject", offsetofbase<CArray<CRect,CRect*>, CObject>()},

EEex-v2.6.6.0/source/EEex-v2.6.6.0/Generated/EEexLua_generated_baseclass_offsets.cpp
@@ -11,9 +11,6 @@ void registerBaseclasses() {
 		{"CResWorldMap", {
 			{"CRes", offsetofbase<CResWorldMap, CRes>()},
 		}},
-		{"CResWebm", {
-			{"CRes", offsetofbase<CResWebm, CRes>()},
-		}},
 		{"CResWave", {
 			{"CRes", offsetofbase<CResWave, CRes>()},
 		}},
@@ -82,6 +79,9 @@ void registerBaseclasses() {
 		}},
 		{"CResArea", {
 			{"CRes", offsetofbase<CResArea, CRes>()},
+		}},
+		{"CResWebm", {
+			{"CRes", offsetofbase<CResWebm, CRes>()},
 		}},
 		{"CArray<CRect,CRect*>", {
 			{"CObject", offsetofbase<CArray<CRect,CRect*>, CObject>()},

EEex-v2.6.6.0/EEex-v2.6.6.0.vcxproj
@@ -19,6 +19,7 @@
     <ClCompile Include="source\EEex-v2.6.6.0\Generated\EEexLua_generated.cpp" />
     <ClCompile Include="source\EEex-v2.6.6.0\Generated\EEexLua_generated_baseclass_offsets.cpp" />
     <ClCompile Include="source\EEex-v2.6.6.0\main.cpp" />
+    <ClCompile Include="source\EEex-v2.6.6.0\menu_util.cpp" />
     <ClCompile Include="source\EEex-v2.6.6.0\time_util.cpp" />
     <ClCompile Include="source\EEex-v2.6.6.0\uncap_fps.cpp" />
     <ClCompile Include="source\EEex-v2.6.6.0\util.cpp" />
@@ -29,6 +30,7 @@
     <ClInclude Include="headers\EEex-v2.6.6.0\coordinate_util.hpp" />
     <ClInclude Include="headers\EEex-v2.6.6.0\EEex.h" />
     <ClInclude Include="headers\EEex-v2.6.6.0\EEexLua_generated.h" />
+    <ClInclude Include="headers\EEex-v2.6.6.0\menu_util.hpp" />
     <ClInclude Include="headers\EEex-v2.6.6.0\time_util.hpp" />
     <ClInclude Include="headers\EEex-v2.6.6.0\uncap_fps.hpp" />
     <ClInclude Include="headers\EEex-v2.6.6.0\util.hpp" />

EEex-v2.6.6.0/EEex-v2.6.6.0.vcxproj.filters
@@ -72,6 +72,9 @@
     <ClCompile Include="source\EEex-v2.6.6.0\coordinate_util.cpp">
       <Filter>Source Files\EEex-v2.6.6.0</Filter>
     </ClCompile>
+    <ClCompile Include="source\EEex-v2.6.6.0\menu_util.cpp">
+      <Filter>Source Files\EEex-v2.6.6.0</Filter>
+    </ClCompile>
   </ItemGroup>
   <ItemGroup>
     <ClInclude Include="headers\EEex-v2.6.6.0\util.hpp">
@@ -99,6 +102,9 @@
       <Filter>Header Files\EEex-v2.6.6.0</Filter>
     </ClInclude>
     <ClInclude Include="headers\EEex-v2.6.6.0\uncap_fps.hpp">
+      <Filter>Header Files\EEex-v2.6.6.0</Filter>
+    </ClInclude>
+    <ClInclude Include="headers\EEex-v2.6.6.0\menu_util.hpp">
       <Filter>Header Files\EEex-v2.6.6.0</Filter>
     </ClInclude>
   </ItemGroup>

EEex-v2.6.6.0/headers/EEex-v2.6.6.0/EEex.h
@@ -50,6 +50,7 @@ namespace EEex {
 	int Override_CGameEffect_CheckSave(CGameEffect* pEffect, CGameSprite* pSprite, byte* saveVSDeathRollRaw, byte* saveVSWandsRollRaw, byte* saveVSPolyRollRaw, byte* saveVSBreathRollRaw, byte* saveVSSpellRollRaw, byte* resistMagicRollRaw);
 	void __cdecl Override_chWriteInifile();
 	//int __cdecl Override_fontWrap(char* text, SDL_Rect* r, SDL_Rect* rClip, int* horizontalAlignment, int* verticalAlignment, font_t* font, int* pointSize, letter_t* letters, int* nlines, int* nletters, int* pointIndex, bool* scale, adjustmentData_t* adjustData, int indent, bool bUseFontSizeFloor);
+	int __cdecl Override_Infinity_TransitionMenu(lua_State* L);
 	int __cdecl Override_Infinity_WriteINILine(lua_State* L);
 	void __cdecl Override_uiDoFile(char* fileName);
 	bool __cdecl Override_uiEventMenuStack(SDL_Event* pEvent, SDL_Rect* pWindow);
@@ -177,6 +178,7 @@ namespace EEex {
 	void UncapFPS_Hook_OnAfterAreaEdgeScrollPossiblyStarted(CGameArea* pArea);
 	void UncapFPS_Hook_OnBeforeWorldScreenDeactivated();
 	void UncapFPS_Hook_HandleAreaAutoZoom();
+	void UncapFPS_Hook_HandleTransitionMenuFade();
 
 	////////////////////
 	// Visibility Map //

EEex-v2.6.6.0/headers/EEex-v2.6.6.0/menu_util.hpp
@@ -1,0 +1,11 @@
+
+#pragma once
+
+#include "Baldur-v2.6.6.0_generated.h"
+
+//-----------------------------//
+//          Functions          //
+//-----------------------------//
+
+int getMenuStackTop();
+uiMenu* getStackMenu(int num);

EEex-v2.6.6.0/headers/EEex-v2.6.6.0_generated/Baldur-v2.6.6.0_generated.h
@@ -135,6 +135,7 @@ struct CPathNode;
 struct CPlex;
 struct CPoint;
 struct CProjectile;
+struct CRect;
 struct CRes;
 template<class RES_CLASS, int RES_ID>
 struct CResHelper;
@@ -189,6 +190,7 @@ struct UI_PanelHeader_st;
 struct WED_LayerHeader_st;
 struct st_tiledef;
 struct uiItem;
+struct uiMenu;
 struct uiVariant;
 
 struct ALCcontext_struct
@@ -3095,6 +3097,16 @@ struct BAMHEADERV2
 	BAMHEADERV2() = delete;
 };
 
+struct _A92C2F5FC159A4FE55DD6CCAABD58E72
+{
+	int state;
+	int opacity;
+	uiMenu* prevMenu;
+	uiMenu* newMenu;
+
+	_A92C2F5FC159A4FE55DD6CCAABD58E72() = delete;
+};
+
 union _9CC80BF4F2F1300360474CD60BF15E00
 {
 	_6B279AA1C7A281E7C97E085DB9F2DFBB __s0;
@@ -3258,31 +3270,6 @@ struct stbtt_fontinfo
 	int indexToLocFormat;
 
 	stbtt_fontinfo() = delete;
-};
-
-struct CRect : tagRECT
-{
-	CRect() = delete;
-};
-
-struct CRainStorm
-{
-	CRainDrop* m_pRainDrops;
-	unsigned __int16 m_nCurrentDensity;
-	CRect m_rOldWorldViewPort;
-	__int16 m_nWindGustCounter;
-	int m_nRainDrops;
-
-	CRainStorm() = delete;
-};
-
-struct CSnowStorm
-{
-	CSnowFlake* m_pSnowFlakes;
-	unsigned __int16 m_nCurrentDensity;
-	CRect m_rOldWorldViewPort;
-
-	CSnowStorm() = delete;
 };
 
 struct CSize : tagSIZE
@@ -4902,32 +4889,6 @@ struct CResWorldMap : CRes
 	CResWorldMap() = delete;
 };
 
-struct CResWebm : CRes
-{
-	struct vtbl : CRes::vtbl
-	{
-		vtbl() = delete;
-	};
-
-	void* m_pCodec;
-	unsigned int m_nFirstFrameTime;
-	unsigned int m_nFrameWaitTime;
-	bool m_bComplete;
-	bool m_bLooping;
-	CRect m_rRender;
-	int m_texture;
-	bool m_bGreyscale;
-	bool m_bTiled;
-	int m_nTransparent;
-	bool m_bUseTone;
-	float m_nDeltaX;
-	float m_nDeltaY;
-	float m_nOffsetX;
-	float m_nOffsetY;
-
-	CResWebm() = delete;
-};
-
 struct CResWave : CRes
 {
 	struct vtbl : CRes::vtbl
@@ -5240,6 +5201,65 @@ struct CResArea : CRes
 	};
 
 	CResArea() = delete;
+};
+
+struct CRect : tagRECT
+{
+	CRect() = delete;
+
+	CRect(int left, int top, int right, int bottom)
+	{
+		this->left = left;
+		this->top = top;
+		this->right = right;
+		this->bottom = bottom;
+	}
+};
+
+struct CRainStorm
+{
+	CRainDrop* m_pRainDrops;
+	unsigned __int16 m_nCurrentDensity;
+	CRect m_rOldWorldViewPort;
+	__int16 m_nWindGustCounter;
+	int m_nRainDrops;
+
+	CRainStorm() = delete;
+};
+
+struct CSnowStorm
+{
+	CSnowFlake* m_pSnowFlakes;
+	unsigned __int16 m_nCurrentDensity;
+	CRect m_rOldWorldViewPort;
+
+	CSnowStorm() = delete;
+};
+
+struct CResWebm : CRes
+{
+	struct vtbl : CRes::vtbl
+	{
+		vtbl() = delete;
+	};
+
+	void* m_pCodec;
+	unsigned int m_nFirstFrameTime;
+	unsigned int m_nFrameWaitTime;
+	bool m_bComplete;
+	bool m_bLooping;
+	CRect m_rRender;
+	int m_texture;
+	bool m_bGreyscale;
+	bool m_bTiled;
+	int m_nTransparent;
+	bool m_bUseTone;
+	float m_nDeltaX;
+	float m_nDeltaY;
+	float m_nOffsetX;
+	float m_nOffsetY;
+
+	CResWebm() = delete;
 };
 
 struct CPoint
@@ -7839,6 +7859,9 @@ extern type_DrawClear p_DrawClear;
 typedef uint (*type_DrawColor)(uint color);
 extern type_DrawColor p_DrawColor;
 
+typedef void (*type_DrawDisable)(DrawFeature f);
+extern type_DrawDisable p_DrawDisable;
+
 typedef void (*type_DrawEnable)(DrawFeature f);
 extern type_DrawEnable p_DrawEnable;
 
@@ -7847,6 +7870,9 @@ extern type_DrawPopState p_DrawPopState;
 
 typedef void (*type_DrawPushState)();
 extern type_DrawPushState p_DrawPushState;
+
+typedef void (*type_DrawQuad)(const CRect* rDest, const CRect* rClip);
+extern type_DrawQuad p_DrawQuad;
 
 typedef void (*type_drawSlice)(const SDL_Rect* dr, const SDL_Rect* r, const SDL_Rect* rClip, float scaleX, float scaleY, bool unused);
 extern type_drawSlice p_drawSlice;
@@ -7902,6 +7928,12 @@ extern type_uiDrawSlicedRect p_uiDrawSlicedRect;
 typedef int (*type_uiExecLuaInt)(int id);
 extern type_uiExecLuaInt p_uiExecLuaInt;
 
+typedef bool (*type_uiPop)(const char* name);
+extern type_uiPop p_uiPop;
+
+typedef bool (*type_uiPush)(const char* name);
+extern type_uiPush p_uiPush;
+
 typedef int (*type_uiVariantAsInt)(uiVariant* var);
 extern type_uiVariantAsInt p_uiVariantAsInt;
 
@@ -7924,6 +7956,7 @@ extern Array<uiMenu*,256>* p_menuStack;
 extern int* p_nextStackMenuIdx;
 extern int* p_numMenus;
 extern CTypedPtrArray<CPtrArray,CRes*>* p_resources;
+extern _A92C2F5FC159A4FE55DD6CCAABD58E72* p_transition;
 extern ConstArray<ushort,1765>* p_yy_action;
 extern ConstArray<ushort,329>* p_yy_default;
 extern ConstArray<byte,1765>* p_yy_lookahead;

commit a26a842da496ab7953998fa72e0554bf25a52514

Fix CRect binding regression in b67cf26

EEex-v2.6.6.0/source/EEex-v2.6.6.0/Generated/EEexLua_generated.cpp
@@ -494,6 +494,7 @@ static void tolua_reg_types(lua_State* L)
 	tolua_usertype(L, "CDungeonMaster");
 	tolua_usertype(L, "uiItem");
 	tolua_usertype(L, "CResBinary");
+	tolua_usertype(L, "EngineGlobals");
 	tolua_usertype(L, "CSteam");
 	tolua_usertype(L, "EEex_CScreenMovies");
 	tolua_usertype(L, "EEex_CScreenConnection");
@@ -1275,6 +1276,9 @@ int OpenBindingsInternal(lua_State* L)
 	tolua_endmodule(L);
 	tolua_cclass(L, "CResBinary", "CResBinary", {"CRes"}, NULL);
 	tolua_beginmodule(L, "CResBinary");
+	tolua_endmodule(L);
+	tolua_cclass(L, "EngineGlobals", "EngineGlobals", {}, NULL);
+	tolua_beginmodule(L, "EngineGlobals");
 	tolua_endmodule(L);
 	tolua_cclass(L, "CSteam", "CSteam", {}, NULL);
 	tolua_beginmodule(L, "CSteam");

LuaBindings-v2.6.6.0/source/LuaBindings-v2.6.6.0/Generated/EEexLua_generated_baseclass_offsets.cpp
@@ -167,6 +167,9 @@ void registerBaseclasses() {
 		{"CResArea::vtbl", {
 			{"CRes::vtbl", offsetofbase<CResArea::vtbl, CRes::vtbl>()},
 		}},
+		{"CRect", {
+			{"tagRECT", offsetofbase<CRect, tagRECT>()},
+		}},
 		{"CResWebm", {
 			{"CRes", offsetofbase<CResWebm, CRes>()},
 		}},

scripts/generate_bindings/generate_bindings.py
@@ -4433,9 +4433,8 @@ def writeBindings(mainState: MainState, outputFileName: str, groups: UniqueList[
 		for enumTuple in group.enumTuples:
 			handleEnumTuple(enumTuple)
 
-		# Empty structs (those that simply derive from a superclass) should
-		# be outputted if they were defined when bindings were enabled.
-		if len(group.fields) == 0 and len(group.functionImplementations) == 0 and not group.lineGroupFlags.isFlagged("noBindings"):
+		# Always output structs if they are used and were defined when bindings were enabled.
+		if not group.lineGroupFlags.isFlagged("noBindings"):
 			groupOpenData.hasReasonForOutput = True
 
 		#handleHardcodedBindings()