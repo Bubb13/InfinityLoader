c76588878f36b707e519738136347960ff2edc8a
Bubb13 <36863623+Bubb13@users.noreply.github.com>
2026-09-20 16:34:17 -0700

    Disable PST:EE in the collective header generation script

--- scripts/generate_bindings/RUN_GENERATE_HEADER_ALL.bat
+++ scripts/generate_bindings/RUN_GENERATE_HEADER_ALL.bat
@@ -3,7 +3,7 @@ pushd "%~dp0"
 
 start cmd.exe /c "call ../../EEex-v2.6.6.0/scripts/generate_bindings/RUN_GENERATE_HEADER.bat"
 start cmd.exe /c "call ../../EEex-v2.7.3.0/scripts/generate_bindings/RUN_GENERATE_HEADER.bat"
-start cmd.exe /c "call ../../LuaBindings-PSTEE/scripts/generate_bindings/RUN_GENERATE_HEADER.bat"
+rem start cmd.exe /c "call ../../LuaBindings-PSTEE/scripts/generate_bindings/RUN_GENERATE_HEADER.bat"
 start cmd.exe /c "call ../../LuaBindings-v2.6.6.0/scripts/generate_bindings/RUN_GENERATE_HEADER.bat"
 start cmd.exe /c "call ../../LuaBindings-v2.7.3.0/scripts/generate_bindings/RUN_GENERATE_HEADER.bat"
 
